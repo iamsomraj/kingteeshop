c692c8f15ee2844cb3c5d49b3da6a3ba8c56c842
Somraj Mukherjee <iamsomraj@gmail.com>
2024-12-24 21:16:59 +0530

    feat: :sparkles: implement dynamic filters component with checkbox functionality

--- components/filters.vue
+++ components/filters.vue
@@ -1,13 +1,62 @@
 <template>
-    <div>
-        filters
+  <div class="flex flex-col gap-4">
+    <div
+      v-for="item in filters"
+      :key="item.key"
+      class="flex flex-col gap-2 items-start"
+    >
+      <div class="font-semibold">{{ item.key }}</div>
+      <div
+        :key="value"
+        v-for="value in item.values"
+        class="flex gap-2 items-center"
+      >
+        <input
+          type="checkbox"
+          :id="value"
+          :value="value"
+          :checked="filter[item.key].includes(value)"
+          @input="checkboxHandler(item.key, value)"
+        />
+        <label :for="value">{{ value }}</label>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+const props = defineProps<{
+  criteria: Record<string, string[]>;
+  filter: Record<string, string[]>;
+}>();
 
+const emit = defineEmits({
+  'update:filter': (value: Record<string, string[]>) => true,
+});
+
+const filters = computed(() => {
+  return Object.entries(props.criteria).map(([key, values]) => {
+    return {
+      key,
+      values,
+    };
+  });
+});
+
+const checkboxHandler = (key: string, value: string) => {
+  const index = props.filter[key].indexOf(value);
+  if (index === -1) {
+    emit('update:filter', {
+      ...props.filter,
+      [key]: [...props.filter[key], value],
+    });
+  } else {
+    emit('update:filter', {
+      ...props.filter,
+      [key]: props.filter[key].filter((_, i) => i !== index),
+    });
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="flex gap-4">
-    <filters />
+    <filters
+      :criteria="itemFilterCriteria"
+      :filter="form.filter"
+      @update:filter="form.filter = $event"
+    />
     <products-container />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+type FormData = {
+  filter: Record<string, string[]>;
+};
+const itemFilterCriteria = {
+  Color: ['Red', 'Blue', 'Green'],
+  Gender: ['Man', 'Woman'],
+  Price: ['0 - Rs 250', 'Rs 251 - 450', 'Rs 450'],
+  Type: ['Polo', 'Hoodie', 'Basic'],
+};
+
+const form = reactive({
+  filter: { Color: [], Gender: [], Price: [], Type: [] },
+}) as FormData;
+</script>
 
 <style scoped></style>

a462d165773f5428076d729fb604b644204c9887
Somraj Mukherjee <iamsomraj@gmail.com>
2024-12-25 02:00:51 +0530

    feat: :sparkles: improve product display layout with responsive grid and enhanced styling

--- components/products/product-list.vue
+++ components/products/product-list.vue
@@ -1,26 +1,23 @@
 <template>
-  <div class="grid grid-cols-3">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
     <div
       v-for="product in products"
       :key="product.id"
-      class="flex flex-col gap-4"
+      class="flex flex-col gap-4 border rounded"
     >
-      <div class="flex gap-4">
-        <img
-          :src="product.imageURL"
-          :alt="product.name"
-          class="w-32 h-32 object-cover rounded-lg"
-        />
-        <div class="flex flex-col gap-2">
+      <div class="flex flex-col gap-4 divide-y">
+        <div class="mx-auto p-4">
+          <img
+            :src="product.imageURL"
+            :alt="product.name"
+            class="w-64 h-64 object-cover rounded-lg"
+          />
+        </div>
+        <div class="flex flex-col gap-2 p-4">
           <div class="text-lg font-semibold">{{ product.name }}</div>
           <div class="text-sm">{{ product.type }}</div>
           <div class="text-sm">{{ product.color }}</div>
-          <div
-            class="text-sm"
-            :class="{ 'text-red-500': product.price >= 450, 'text-green-500': product.price <= 250 }"
-          >
-            {{ product.price }} {{ product.currency }}
-          </div>
+          <div class="text-sm">{{ product.price }} {{ product.currency }}</div>
         </div>
       </div>
     </div>

051ef01ef639e9f75db617075643db1d08d946ef
Somraj Mukherjee <iamsomraj@gmail.com>
2024-12-25 16:39:06 +0530

    feat: :sparkles: add empty cart message and conditional rendering in cart component

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="flex flex-col gap-4">
+  <div
+    v-if="shirtsStore.cart.length > 0"
+    class="flex flex-col gap-4"
+  >
     <cart-item
       v-for="cart in shirtsStore.computedCart"
       :key="cart.id"
@@ -8,6 +11,16 @@
       @remove:cart="shirtsStore.removeFromCart"
       @delete:cart="shirtsStore.deleteFromCart"
     />
+  </div>
+  <div
+    v-else
+    class="flex items-center justify-center h-96 gap-4"
+  >
+    <Icon
+      name="material-symbols:shopping-cart"
+      class="w-12 h-12"
+    />
+    <div class="text-2xl font-semibold">Cart is empty</div>
   </div>
 </template>
 

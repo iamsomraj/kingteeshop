b329d9a1cb730d9ce407f8eb7ae1bb8a21083098
Somraj Mukherjee <iamsomraj@gmail.com>
2024-12-25 19:42:49 +0530

    feat: :sparkles: add total price calculation and checkout button to cart component

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -1,16 +1,29 @@
 <template>
   <div
     v-if="shirtStore.cart.length > 0"
-    class="flex flex-col gap-4"
+    class="flex flex-col gap-8 mb-20"
   >
-    <cart-item
-      v-for="cart in shirtStore.computedCart"
-      :key="cart.id"
-      :cart="cart"
-      @add:cart="shirtStore.addToCart"
-      @remove:cart="shirtStore.removeFromCart"
-      @delete:cart="shirtStore.deleteFromCart"
-    />
+    <div class="flex flex-col gap-4">
+      <cart-item
+        v-for="cart in shirtStore.computedCart"
+        :key="cart.id"
+        :cart="cart"
+        @add:cart="shirtStore.addToCart"
+        @remove:cart="shirtStore.removeFromCart"
+        @delete:cart="shirtStore.deleteFromCart"
+      />
+    </div>
+
+    <div class="flex justify-between items-center gap-4">
+      <div class="text-2xl font-semibold">Total: ${{ total }}</div>
+      <button class="flex items-center gap-2 rounded-lg border-2 border-gray-700 p-2 font-semibold hover:bg-gray-100">
+        <Icon
+          name="material-symbols:attach-money-rounded"
+          class="w-6 h-6 cursor-pointer"
+        />
+        <span>Checkout</span>
+      </button>
+    </div>
   </div>
   <div
     v-else
@@ -26,6 +39,12 @@
 
 <script setup lang="ts">
 const shirtStore = useShirtStore();
+
+const total = computed(() => {
+  return shirtStore.computedCart.reduce((acc, cart) => {
+    return acc + cart.price * cart.quantityInCart;
+  }, 0);
+});
 </script>
 
 <style scoped></style>
